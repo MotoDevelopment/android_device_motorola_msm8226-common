fcbddb70e379afe6b11a5d0ac614c8a031ac1c69
Steve Kondik <shade@chemlab.org>
2013-05-17 13:10:19 -0700

    msm8226-common: Increase heap start size to 16m to minimize GC with larger bitmaps
    Change-Id: I89af5e94c9aafdc6007c84d3fce60e4db349e967

--- msm8226.mk
+++ msm8226.mk
@@ -111,7 +111,7 @@ PRODUCT_PACKAGES += \
 
 # Dalvik properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapstartsize=16m \
     dalvik.vm.heapgrowthlimit=192m \
     dalvik.vm.heapsize=256m \
     dalvik.vm.heaptargetutilization=0.75 \
