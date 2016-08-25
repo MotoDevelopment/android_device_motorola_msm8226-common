0633994961770e17e3f6ddf55271030cdbc1a707
Sultanxda <sultanxda@gmail.com>
2016-08-24 22:01:44 -0700

    msm8226-common: Disable BOARD_CHARGER_ENABLE_SUSPEND
    This is causing wakeups from hell (likely due to spurious USB detections
    in the kernel), when its original intent was to *save* battery life. This
    is causing wakeups even when there is nothing plugged into the USB port,
    so disable it to reclaim our shiny battery life.
    
    Change-Id: Iecc411e24fa4b99482547faad4fffc2b5d7ab94e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -98,7 +98,6 @@ TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
 
 # Charger
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
-BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_NO_CHARGER_LED := true
 
 # Display
