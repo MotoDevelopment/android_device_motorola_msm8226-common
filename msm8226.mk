33b67ae15cf5736121988e4848c3f394c9fe9411
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:23:30 +0200

    msm8226-common: Add HIDL manifest

--- msm8226.mk
+++ msm8226.mk
@@ -132,6 +132,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     gps.msm8226
 
+# HIDL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
