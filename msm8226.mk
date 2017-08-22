328cdf2690a9baa71cc57604d2aac74dda782568
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:10:19 +0200

    msm8226-common: Build DRM HAL

--- msm8226.mk
+++ msm8226.mk
@@ -96,6 +96,10 @@ PRODUCT_PACKAGES += \
     camera.msm8226 \
     Snap
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
