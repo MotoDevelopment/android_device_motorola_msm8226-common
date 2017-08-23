35e08732c99ee4bed06e3cc85dbc80451ed629c5
Simon Shields <simon@lineageos.org>
2017-08-24 00:23:09 +1000

    msm8226-common: CameraWrapper: Add missing libs for headers
    * Also clean this up a bit
    
    Change-Id: Ifdaf82855e9ccc3b9d8e13f4124e3670b803109d

--- camera/Android.mk
+++ camera/Android.mk
@@ -2,17 +2,16 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := \
-    frameworks/native/include \
     system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
+LOCAL_STATIC_LIBRARIES := libarect
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libgui libutils libbase libhidltransport libsensor libutils android.hidl.token@1.0-utils
-
-LOCAL_STATIC_LIBRARIES := \
-    libarect
+    libhardware liblog libcamera_client libgui libutils libbase libhidltransport libsensor \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
 
 LOCAL_HEADER_LIBRARIES := libnativebase_headers
 
