256e2e69785182c7c4e5f47f08e33fff3eaf7bc0
Ethan Chen <intervigil@gmail.com>
2017-09-16 23:19:53 -0700

    msm8226-common: Add Vibrator HAL in place of LineageHW implementation
    Change-Id: I19d4f916d807301ec30b882bfdeaf76715a041c7

--- msm8226.mk
+++ msm8226.mk
@@ -309,7 +309,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-service.moto8226
 
 # WCNSS
 PRODUCT_COPY_FILES += \
