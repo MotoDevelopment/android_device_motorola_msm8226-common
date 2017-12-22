ba3035058eeae9f769a7d5ad8e84e6d8dbcb80f4
Artem Borisov <dedsa2002@gmail.com>
2017-12-22 11:45:58 +0300

    msm8226-common: Dex-preopt boot and system server
    WITH_DEXPREOPT_BOOT_IMG_ONLY was removed in 8.1 in favor of
    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY.
    
    Along with boot classpath it will also pre-optimize system server jars.
    
    Change-Id: Idd0ac1ac39a6c3d14637a974a0a99953c99af236

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -131,10 +131,10 @@ ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_ONLY := true
     endif
   endif
 endif
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 
 # SELinux
 -include device/qcom/sepolicy/sepolicy.mk
