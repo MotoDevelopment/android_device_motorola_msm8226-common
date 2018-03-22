8ac05e2f11988f4a951e8d03eeece3e452c71a00
Michael Bestas <mkbestas@lineageos.org>
2018-03-22 19:25:57 +0200

    msm8226-common: Remove dexpreopt configuration
    * It's set globally now
    
    Change-Id: Ie61179cd898778190bcbec15779dbdca4070bfba

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -156,18 +156,10 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 SF_START_GRAPHICS_ALLOCATOR_SERVICE := true
 
-# Basic dexpreopt
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
+# Dexpreopt
 WITH_DEXPREOPT_DEBUG_INFO := false
 USE_DEX2OAT_DEBUG := false
 DONT_DEXPREOPT_PREBUILTS := true
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 
 # SELinux
 include device/qcom/sepolicy-legacy/sepolicy.mk
