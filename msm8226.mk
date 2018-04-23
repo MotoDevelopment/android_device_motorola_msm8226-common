794912128e5e3eb34fc4b963b8e038bf322b28c3
RenanQueiroz <queirozrrq@gmail.com>
2018-04-22 20:31:15 -0400

    msm8226-common: camera: Fix HAL1 flags
    Signed-off-by: RenanQueiroz <queirozrrq@gmail.com>

--- msm8226.mk
+++ msm8226.mk
@@ -93,7 +93,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl \
     camera.msm8226 \
     libxml2 \
     libshims_camera \
