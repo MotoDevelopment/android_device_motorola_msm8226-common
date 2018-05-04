43facacfb2aa2eb4df5263a070aff487dd37f6df
Ethan Chen <intervigil@gmail.com>
2018-05-03 22:26:41 -0700

    msm8226-common: Build LegacyMM livedisplay HAL
    Change-Id: Ic6609e573d56a69bbd9d610ad1c84a1870428428

--- msm8226.mk
+++ msm8226.mk
@@ -184,6 +184,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     lights.msm8226
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-legacymm
+
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/vendor/etc/media_codecs_google_audio.xml \
