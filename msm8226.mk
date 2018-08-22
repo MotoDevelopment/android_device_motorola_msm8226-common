90676b659fb260e28183bd840d61ad6bc48d85c8
Kevin Rocard <krocard@google.com>
2018-08-21 22:42:45 -0700

    msm8226-common: Migrate to audio V4 implementation
    Test: regression test on the board
    Bug: 38184704
    Change-Id: Ife559125850277865451f92ea862e7ec47fc9da7
    Signed-off-by: Subinsmani <subins.mani@gmail.com>

--- msm8226.mk
+++ msm8226.mk
@@ -96,8 +96,8 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl
 
