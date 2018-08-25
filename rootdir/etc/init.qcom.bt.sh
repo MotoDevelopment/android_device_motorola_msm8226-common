8d8a843ffc6ab1bad84f6245185ef96b3b656653
dianlujitao <dianlujitao@lineageos.org>
2018-08-25 19:07:05 +0800

    msm8226-common: Rename bt props for new HAL
    * Also move some stuff to vendor
    
    Change-Id: I1afd110bffed0f43b1afcbef99be8a3776d24e0b

--- rootdir/etc/init.qcom.bt.sh
+++ rootdir/etc/init.qcom.bt.sh
@@ -53,7 +53,7 @@ failed ()
 
 program_bdaddr ()
 {
-  /system/bin/btnvtool -O
+  /system/vendor/bin/btnvtool -O
   logi "Bluetooth Address programmed successfully"
 }
 
@@ -69,7 +69,7 @@ config_bt ()
   else
     soc_hwid=`cat /sys/devices/system/soc/soc0/id`
   fi
-  btsoc=`getprop qcom.bluetooth.soc`
+  btsoc=`getprop vendor.qcom.bluetooth.soc`
 
   case $baseband in
     "apq")
@@ -250,7 +250,7 @@ case $STACK in
     ;;
     *)
        logi "** Bluedroid stack **"
-       setprop bluetooth.status off
+       setprop vendor.bluetooth.status off
     ;;
 esac
 
@@ -279,7 +279,7 @@ case $LE_POWER_CLASS in
      logi "LE Power Class: To override, Before turning BT ON; setprop qcom.bt.le_dev_pwr_class <1 or 2 or 3>";;
 esac
 
-eval $(/system/bin/hci_qcomm_init -e $PWR_CLASS $LE_PWR_CLASS && echo "exit_code_hci_qcomm_init=0" || echo "exit_code_hci_qcomm_init=1")
+eval $(/system/vendor/bin/hci_qcomm_init -e $PWR_CLASS $LE_PWR_CLASS && echo "exit_code_hci_qcomm_init=0" || echo "exit_code_hci_qcomm_init=1")
 
 case $exit_code_hci_qcomm_init in
   0) logi "Bluetooth QSoC firmware download succeeded, $BTS_DEVICE $BTS_TYPE $BTS_BAUD $BTS_ADDRESS";;
@@ -290,7 +290,7 @@ case $exit_code_hci_qcomm_init in
          ;;
          *)
             logi "** Bluedroid stack **"
-            setprop bluetooth.status off
+            setprop vendor.bluetooth.status off
         ;;
      esac
 
@@ -309,7 +309,7 @@ case $TRANSPORT in
            ;;
            *)
               logi "** Bluedroid stack **"
-              setprop bluetooth.status on
+              setprop vendor.bluetooth.status on
            ;;
        esac
      ;;
@@ -322,7 +322,7 @@ case $TRANSPORT in
             ;;
             *)
                logi "** Bluedroid stack **"
-               setprop bluetooth.status on
+               setprop vendor.bluetooth.status on
             ;;
         esac
 
