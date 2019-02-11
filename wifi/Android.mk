70a591ef3c762b8c0244d7a1afc518e6fa9a72cd
Michael Bestas <mkbestas@lineageos.org>
2019-02-11 15:35:46 +0200

    msm8226-common: Stop copying wifi configs to userdata
    Change-Id: I7b9fbc0e26a1563f3e13fe0f9d58308b618a5702

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -21,11 +21,6 @@ LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/wifi
 include $(BUILD_PREBUILT)
 
-# Create symbolic links
-$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
-	ln -sf /data/vendor/wifi/WCNSS_qcom_cfg.ini \
-	$(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := WCNSS_qcom_wlan_factory_nv.bin
 LOCAL_MODULE_CLASS := FAKE
