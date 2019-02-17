efd382b8d172e786af9e6d34df3268aa91b0bac6
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-17 17:11:09 -0300

    msm8226-common: Update cache partition path

--- rootdir/etc/dump_pds.sh
+++ rootdir/etc/dump_pds.sh
@@ -1,9 +1,9 @@
 #!/system/bin/sh
 
-DUMP=/cache/pds-dump.img
+DUMP=/data/cache/pds-dump.img
 
 if [ ! -f $DUMP ]; then
-    # Make a copy of pds in /cache
+    # Make a copy of pds in /data/cache
     cp /dev/block/platform/msm_sdcc.1/by-name/pds $DUMP-temp
     # Prevent init from using a partially created file
     mv $DUMP-temp $DUMP
